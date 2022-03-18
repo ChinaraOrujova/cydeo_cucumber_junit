69995d2d1c37cfea4f06f0e287ddbbd2783a5927
Chinara Orujova <china.orujova@gmail.com>
2022-03-18 11:07:30 -0400

    Google Search

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -6,10 +6,20 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class GoogleStepDefinitions {
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+
+    }
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+
+    }
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
        String expectedTitle ="Google";
@@ -19,4 +29,7 @@ public class GoogleStepDefinitions {
 
         Driver.closeDriver();
     }
+
+
+
 }
